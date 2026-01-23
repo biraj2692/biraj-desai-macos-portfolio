7ed475c38a63bf0bea5537071cdd02b719e56262
biraj2692 <77262608+biraj2692@users.noreply.github.com>
2026-01-23 18:15:46 +0530

    window store feature

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,12 @@
 import { dockApps } from "#constants";
+import useWindowStore from "#store/windows";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef } from "react";
 import { Tooltip } from "react-tooltip";
 
 const Dock = () => {
+  const { openWindow, closeWindow, windows } = useWindowStore();
   const dockRef = useRef();
 
   useGSAP(() => {
@@ -31,7 +33,7 @@ const Dock = () => {
     };
     const handleMouseMove = (e) => {
       const { left } = dock.getBoundingClientRect();
-      animateIcons((e.clientX - left));
+      animateIcons(e.clientX - left);
     };
     const resetIcon = () =>
       icons.forEach((icon) =>
@@ -45,7 +47,21 @@ const Dock = () => {
     };
   }, []);
   const toggleApp = (app) => {
-    // TODO Implement Open Withdraw
+    if (!app.canOpen) return;
+
+    const window = windows[app.id];
+    
+    if(!window){
+      console.error(`Window not found for app ${app.id}`)
+      return;
+    }
+    
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
+    console.log(windows)
   };
 
   return (
