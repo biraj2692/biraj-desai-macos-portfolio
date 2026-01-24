fe9fa09ef9309be69b4911c0845645d67719db39
biraj2692 <77262608+biraj2692@users.noreply.github.com>
2026-01-24 16:53:19 +0530

    text-image preview and contact information implemented

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,5 +2,9 @@ import Finder from "./Finder";
 import Resume from "./Resume";
 import Safari from "./Safari";
 import Terminal from "./Terminal";
+import TextFile from "./TextFile";
+import ImgFile from "./ImgFile";
+import Contact from "./Contact";
 
-export {Terminal, Safari, Resume, Finder}
+
+export {Contact, Terminal, Safari, Resume, Finder, TextFile, ImgFile }
